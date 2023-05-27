06990fcdb1e2fc283dcf0bb34ce963c91629b585
gilyazov <giliyazovmarat@gmail.com>
2023-05-28 01:17:23 +0300

    init

--- main.py
+++ main.py
@@ -18,8 +18,13 @@ async def startup_event():
 
 @app.post("/predict")
 async def predict_endpoint(item: Item):
-    return await predict_service(item, app.context['prediction'])
+    result = await predict_service(item, app.context['prediction'])
+    print('-------')
+    print(result)
+    print(item.link)
+    print('-------')
+    return result
 
 
 if __name__ == "__main__":
-    uvicorn.run('main:app', host="0.0.0.0", port=8585, workers=10)
+    uvicorn.run('main:app', host="0.0.0.0", port=8585, workers=1)
