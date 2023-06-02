361f03378fb8b5348fb6270540f35c8c40473cd4
gilyazov <giliyazovmarat@gmail.com>
2023-06-02 18:15:39 +0300

    init

--- tester_new.py
+++ tester_new.py
@@ -7,12 +7,16 @@ from skin_module.prediction_v2 import Prediction
 
 def worker():
     prediction = Prediction()
-    st = time.monotonic()
 
     content = requests.get('https://www.tprteaching.com/wp-content/uploads/2022/12/peoples-or-peoples.jpg').content
-
+    st = time.monotonic()
     result = prediction._predict(content)
-    print(result)
+    print(time.monotonic()-st)
+
+    st = time.monotonic()
+    result = prediction._predict(content)
+    print(time.monotonic() - st)
+    # print(result)
 
 if __name__ == '__main__':
     worker()
